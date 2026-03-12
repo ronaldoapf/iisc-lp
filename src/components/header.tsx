416c5981a65ddd0f4d356a9bbf6bfc038d66f352
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-03-12 09:45:24 -0300

    fix: update header styles for improved layout and appearance

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,9 +8,9 @@ export function Header() {
       animate={{ y: 0 }}
       initial={{ y: -100 }}
       transition={{ duration: 0.5 }}
-      className="sticky top-0 z-50 w-full backdrop-blur supports-backdrop-filter:bg-background font-sans"
+      className="sticky top-0 z-50 w-full border-b border-b-gray-200 bg-white font-sans shadow-sm"
     >
-      <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto flex h-20 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link to="/">
           <Logo />
         </Link>
